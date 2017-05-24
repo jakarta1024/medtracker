3db2d223bdaecd6bb544a4606890d759a6c80e0e
Gang Li <li.tzuhsien@outlook.com>
2017-05-24 16:53:15 +0800

    add

---
+++ src/app/medical/records/records.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MetadataModule } from './metadata/metadata.module';
+
+@NgModule({
+  imports: [
+    CommonModule,
+    MetadataModule
+  ],
+  declarations: []
+})
+export class RecordsModule { }
